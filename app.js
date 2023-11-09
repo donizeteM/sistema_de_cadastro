22c93330e4a5295c2af952785a51c5c8772d2e38
Jose_Donizete <106778705+donizeteM@users.noreply.github.com>
2023-11-09 11:32:50 -0300

    Entrada de dados

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema ge gerenciamento de jogos");
 console.log("Projeto esta em andamento");
 // teste de conlose log
 console.log("Envinado para o git hub");
+let nome = prompt("Digite seu nome");
